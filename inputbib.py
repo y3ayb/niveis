006d5d9662e7b9e3dbee0c4164ecea0a0aa4d76e
y3ayb <78046763+y3ayb@users.noreply.github.com>
2021-01-28 09:36:20 -0300

    Update inputbib.py

--- inputbib.py
+++ inputbib.py
@@ -15,27 +15,6 @@ def buttonPrint(ui, p, dir, i, j):
     elif dir == 's': txt.place(x=50+120*j, y=115+120*i, width=30, height=30)
     else: txt.place(x=15+120*j, y=80+120*i, width=30, height=30)
 
-'''def buttonUpPrint(ui, P, i, j):
-    txt = uibib.Button(ui, text='w')
-    if (P.psb[i][j].w == False): txt["state"] = uibib.DISABLED
-    else: txt["command"] = lambda: selectMove (i, j, 'w', ui)
-    txt.place(x=50+120*j, y=45+120*i, width=30, height=30)
-
-def buttonRightPrint(ui, P, i, j):
-    if (P.psb[i][j].d == False): txt = uibib.Label(ui)
-    else: txt = uibib.Button(ui, text='d', command = lambda: selectMove (i, j, 'd', ui))
-    txt.place(x=85+120*j, y=80+120*i, width=30, height=30)
-
-def buttonDownPrint(ui, P, i, j):
-    if (P.psb[i][j].s == False): txt = uibib.Label(ui)
-    else: txt = uibib.Button(ui, text='s', command = lambda: selectMove (i, j, 's', ui))
-    txt.place(x=50+120*j, y=115+120*i, width=30, height=30)
-
-def buttonLeftPrint(ui, P, i, j):
-    if (P.psb[i][j].a == False): txt = uibib.Label(ui)
-    else: txt = uibib.Button(ui, text='a', command = lambda: selectMove (i, j, 'a', ui))
-    txt.place(x=15+120*j, y=80+120*i, width=30, height=30)'''
-
 def printBoard (A, P):
     ui = uibib.head()
     uibib.numberMoves(ui, A.nmove)
